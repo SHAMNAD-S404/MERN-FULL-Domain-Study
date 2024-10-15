0b109a24b1807e2a739bb7b9e17afdda3ae456b0
SHAMNAD-S404 <shamnadsakeer306@gmail.com>
2024-10-15 23:05:36 +0530

    added Set

---
+++ JS/weakSet/ base.js
@@ -0,0 +1,31 @@
+  
+  const obj1 = {name : "naja abo"};
+  const obj2 = {name : "ameen"}
+
+  const names = "faheem"
+
+  const weakSet = new WeakSet()
+
+  weakSet.add(obj1)
+  weakSet.add(obj2)
+
+  console.log( weakSet.has(obj1));
+
+  //******************************************************* */
+
+  //? Question: Add an object {id: 1} to a WeakSet. Then, after deleting the reference to the object, check if the WeakSet still contains the object.
+
+  const obj3 = {id:1010}
+
+  const weakset = new WeakSet()
+
+  weakset.add(obj3)
+
+  console.log(weakset.has(obj3));
+
+  obj3 = null
+
+  console.log(weakset.has(obj3))
+  
+  
+  
