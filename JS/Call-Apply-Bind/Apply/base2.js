264bedf506ffa0bf5478f12b1878c0d5363c78f1
SHAMNAD-S404 <155313225+SHAMNAD-S404@users.noreply.github.com>
2024-10-24 23:55:06 +0530

    Update base2.js

--- JS/Call-Apply-Bind/Apply/base2.js
+++ JS/Call-Apply-Bind/Apply/base2.js
@@ -1,8 +1,9 @@
-//? Find Maximum Number in an Array
+//? Find Maximum Number in an Array 
+//? example for apply
 
 const arr1 = [1, 2, 3];
 const arr2 = [4, 5, 6];
 
-arr1.push.apply(arr1,arr2)
+arr1.push.apply(arr1,arr2);
 
 console.log(arr1);
